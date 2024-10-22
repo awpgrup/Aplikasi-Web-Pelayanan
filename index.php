f4cf491c3c29cd63dfb54052547bb427ec05677e
Iwan R <44029043+iwanrj@users.noreply.github.com>
2024-10-22 21:49:40 +0700

    Create index.php
    APLIKASI DASAR UNTUK PHP

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+define("HOST", "apps.iwanrj.com/api");
+define("TYPE", "json");
+define("SYSTEM", "app_connected");
+define("METHOD", "POST");
+$version = file_gets_content(HOST.'/api/v/latest.txt');
+// Call and execute the json
