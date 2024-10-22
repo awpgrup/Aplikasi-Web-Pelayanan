391ecd46c2aeeef2bff2cc36b6beafc1af43f1dd
Iwan R <44029043+iwanrj@users.noreply.github.com>
2024-10-22 21:50:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ define("HOST", "apps.iwanrj.com/api");
 define("TYPE", "json");
 define("SYSTEM", "app_connected");
 define("METHOD", "POST");
-$version = file_gets_content(HOST.'/api/v/latest.txt');
+$version = file_gets_content(HOST.'/v/latest.txt');
 // Call and execute the json
